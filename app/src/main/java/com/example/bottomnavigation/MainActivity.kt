2e847a33df47a42d8dc2e876b6558ef8b7a92918
sameeksha <sameeksha.mourya@coditas.com>
2022-10-06 19:44:52 +0530

    first commit

--- app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
         mMainNavController.navInflater.inflate(R.navigation.nav_graph_main)
     }
 }
